5997a13609eb87fc3f40afe1736d30fbccb2f44b
ARleywin <arleywin86@gmail.com>
2022-03-27 23:35:54 +0700

    RMX2020: overlay: Introduce Nad Maintainer Overlay

--- device.mk
+++ device.mk
@@ -250,7 +250,8 @@ PRODUCT_COPY_FILES += \
 
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += \
-    $(DEVICE_PATH)/overlay
+    $(DEVICE_PATH)/overlay \
+    $(DEVICE_PATH)/overlay-nad
 
 # Permissions
 PRODUCT_COPY_FILES += \
